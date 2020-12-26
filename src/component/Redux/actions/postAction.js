6d46d23e349db23236de588d5e642b99a0ef8be1
Debayo Tomisin <debayotomisin07@gmail.com>
2020-12-26 05:27:10 -0800

    actions comps

---
+++ src/component/Redux/actions/postAction.js
@@ -0,0 +1,16 @@
+import { FETCH_POSTS, NEW_POST, LOADING } from './types'
+
+const fecthPosts = () => dispatch => {
+    console.log('fetching...')
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+        .then(res => res.json())
+        .then(posts => 
+            dispatch({
+                type:FETCH_POSTS,
+                payload: posts
+            })
+        );
+    
+}
+
+export default fecthPosts
