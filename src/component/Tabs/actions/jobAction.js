0ef3e036c83dd0db4784079857fdf49a49d842c8
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-22 20:28:50 -0800

    playaround with redux

--- src/component/Tabs/actions/jobAction.js
+++ src/component/Tabs/actions/jobAction.js
@@ -2,7 +2,7 @@ import { FETCH_JOBS, FETCH_COMPANY} from '../actions/types'
 
 const url = 'https://course-api.com/react-tabs-project'
 
-export const fetchJobs = () => dispatch => {
+export const fetchJobs = () => dispatch => { //promises method
     fetch(url)
     .then(res => res.json())
     .then(data => 
@@ -13,7 +13,7 @@ export const fetchJobs = () => dispatch => {
 }
 
 
-const fetchJob = async dispatch => {
+const fetchJob = async dispatch => { // async await method
     const response = await fetch(url)
     const data = response.json()
     dispatch({
